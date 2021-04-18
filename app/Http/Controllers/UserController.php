18fd3b709bc2fed0c02a237cda565c172fbe1f59
yeshi <Yeshi@DESKTOP-JJA5H9E>
2021-04-18 16:21:27 +0530

    login functionality, middleware, session

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function login(Request $request)
+    {
+        $user = User::where(['email' => $request->email])->first();
+        if(!$user || !Hash::check($request->password, $user->password))
+        {
+            return 'Email or Password not matched.';
+        }
+        else 
+        {
+            $request->session()->put('user', $user);
+            return redirect('/');
+        }
+    }
+}
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -6,15 +6,16 @@
     <div class="row">
         {{-- to center the form --}}
         <div class="col-sm-4 col-sm-offset-4">
-            <form>
+            <form action="login" method="POST">
                 <div class="form-group">
+                    @csrf
                   <label for="exampleInputEmail1">Email address</label>
-                  <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                  <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
                   <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
                 </div>
                 <div class="form-group">
                   <label for="exampleInputPassword1">Password</label>
-                  <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                  <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
                 </div>
                 <button type="submit" class="btn btn-primary">Submit</button>
               </form>

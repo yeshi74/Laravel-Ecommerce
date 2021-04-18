078d1394bd7a96930df3b2115bc0e78d64438216
yeshi <Yeshi@DESKTOP-JJA5H9E>
2021-04-18 14:50:50 +0530

    header footer and login page

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,10 @@
+@section('footer')
+<footer class="bg-light text-center text-lg-start">
+    <!-- Copyright -->
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+      © 2020 Copyright:
+      <a class="text-dark" href="https://mdbootstrap.com/">Larablog.com</a>
+    </div>
+    <!-- Copyright -->
+  </footer>
+  @endsection
